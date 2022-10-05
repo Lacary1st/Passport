4120e20fd160e96201a0b48243c49731262142c3
Oluwatomilola <oluwatomilola22@gmail.com>
2022-10-05 08:11:08 -1200

    wrote html css

---
+++ script.js
@@ -0,0 +1,8 @@
+function name(){
+    let result;
+    let name = document.getElementById("name").value
+    result= ('text')
+    result= name.value
+
+    document.getElementById('fullname').innerHTML= result
+}
