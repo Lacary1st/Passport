e1e04a6d45cfa3f1124dc1020af9460f7cb71949
Oluwatomilola <oluwatomilola22@gmail.com>
2022-10-05 23:05:31 -1200

    checking the invalid phone number section

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-function name(){
+function myName(){
     let result;
-    let name = document.getElementById("name").value
-    result= ('text')
+    let name=document.getElementById("name")
     result= name.value
-
-    document.getElementById('fullname').innerHTML= result
+    document.getElementById('fullname').innerHTML= result;
 }
+
+function myNumber(){
+    let validNumber; 
+    let number =document.getElementById("num")
+    input =number.value
+if (input.startsWith('070') || input.startsWith('080') || input.startsWith('081') || input.startsWith('090') || input.startsWith('091')){
+    if ( input.length == 11 ){
+        validNumber = input
+    
+    }
+    else {
+             validNumber = 'Invalid'
+          }
+        }
+          document.getElementById('telephone').innerHTML= validNumber;
+    
+}
+
+   
+function myAge(){
+    let result;
+    let age=document.getElementById("age")
+    result= age.value
+    document.getElementById('dateofbirth').innerHTML= result;
+}
+
+
+function myPob(){
+    let result;
+    let pob =document.getElementById("pob")
+    result= pob.value
+    document.getElementById('place').innerHTML= result;
+}
+
+
+function myEmail(){
+    let result;
+    let email=document.getElementById("email")
+    result= email.value
+    document.getElementById('electronicmail').innerHTML= result;
+}
+
